4f0b1b6b82da44b5a8e77424accea546a14d9a79
jwa <jonas.wanner@clavisit.com>
2020-09-09 16:22:34 +0200

    fixing getStore method with stateExtender

--- vuejs-vuex-portlet/src/store/index.js
+++ vuejs-vuex-portlet/src/store/index.js
@@ -3,15 +3,31 @@ import Vuex from 'vuex/dist/vuex.common'
 
 Vue.use(Vuex)
 
+/**
+ * The state object of the vuex instance
+ */
+const state = {
+    count: 0
+}
+
+/**
+ * The mutations object which contains all the mutations methods of the vuex instance
+ */
+const mutations = {
+    increment: state => state.count++,
+    decrement: state => state.count--
+}
+
+/**
+ * Gets the store with, if wanted an extended state by passed
+ * params
+ *
+ * @param stateExtender, to extend the state with variables which are only available on runtime
+ * @returns {Store}
+ */
 export const getStore = (stateExtender = {}) => {
     return new Vuex.Store({
-        state: {
-            count: 0,
-            ...stateExtender
-        },
-        mutations: {
-            increment: state => state.count++,
-            decrement: state => state.count--
-        }
+        state: Object.assign(state, stateExtender),
+        mutations: mutations
     })
 }
