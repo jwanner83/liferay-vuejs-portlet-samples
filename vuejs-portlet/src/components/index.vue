2c91f0b6cce242f80f9802feb0e194edbb878a96
jwa <jonas.wanner@clavisit.com>
2021-02-23 18:55:45 +0100

    adding js pdf

--- vuejs-portlet/src/components/index.vue
+++ vuejs-portlet/src/components/index.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="portlet__wrapper container-fluid-1280 main-content-body">
     <div class="portlet sheet">
-      <h1 class="portlet__title">Vue.js Portlet</h1>
+      <h1 class="portlet__title">jsPDF Portlet</h1>
       <p class="portlet__version">Version: {{ version }}</p>
+      <button class="btn btn-primary" @click="print">Print</button>
       <configuration />
     </div>
   </div>
 </template>
 
 <script>
+import jsPDF from 'jspdf'
 import Configuration from './configuration/index.vue'
 
 export default {
@@ -19,6 +21,15 @@ export default {
   computed: {
     version () {
       return '1.1.0'
+    }
+  },
+  methods: {
+    print () {
+      // eslint-disable-next-line new-cap
+      const doc = new jsPDF()
+      doc.html(document.body)
+      doc.autoPrint()
+      doc.output('dataurlnewwindow')
     }
   }
 }
